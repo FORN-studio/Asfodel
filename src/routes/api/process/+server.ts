df751a4daa17095613d769bde3e3c8271a9049b7
akhefale <adrianelias@protonmail.com>
2025-07-30 23:47:17 +0200

    age trees slower

--- src/routes/api/process/+server.ts
+++ src/routes/api/process/+server.ts
@@ -12,7 +12,7 @@ export const GET: RequestHandler = async () => {
         await engine.processNextAgent();
 
         if (Math.random() < 0.03)  await engine.spawnEnergyPacket();
-        if (Math.random() < 0.10)  await engine.ageAllTrees();
+        if (Math.random() < 0.04)  await engine.ageAllTrees();
         if (Math.random() < 0.05)  await engine.cleanupExpiredEvents();
         if (Math.random() < 0.10)  await engine.cleanupStuckProcessingStates();
       } catch (err) {
